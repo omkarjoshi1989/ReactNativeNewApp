8007f00a5e3c5bcaf44833cc4c9b255c5594da85
Omkar <omkarjoshi1989@gmail.com>
2024-06-03 22:09:45 +0530

    indent code with control plus end of square bracket

--- App.tsx
+++ App.tsx
@@ -1,22 +1,22 @@
-import React from "react"
-import {
-View,
-Text,
-SafeAreaView,
-TextInput
-} from 'react-native'
+  import React from "react"
+  import {
+  View,
+  Text,
+  SafeAreaView,
+  TextInput
+  } from 'react-native'
 
-function App(){
-return(
-  <SafeAreaView>
-    <View>
-    <Text>Hello React Native World into this !</Text>
-      <Text>This is my first react native programming..</Text>
-      <TextInput>Name is something ew have</TextInput>
-      <TextInput>Password is not empty</TextInput>
-    </View>
-  </SafeAreaView>
-)
-}
+  function App(){
+  return(
+    <SafeAreaView>
+      <View>
+      <Text>Hello React Native World into this !</Text>
+        <Text>This is my first react native programming..</Text>
+        <TextInput>Name is something ew have</TextInput>
+        <TextInput>Password is not empty</TextInput>
+      </View>
+    </SafeAreaView>
+  )
+  }
 
-export default App
+  export default App
